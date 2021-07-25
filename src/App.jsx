b7b27c2ae8d6d65da1e061ae5157318f95ef9ba3
Sneha Shinde <snehashinde9145@gmail.com>
2021-07-26 00:10:51 +0530

    Initialization

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-6xl">Hello</h1>
+      <h1 className="text-base">World</h1>
+    </div>
+  );
+}
+
+export default App;
